2f5e536e59d15e19652021bd2b6002369a55970d
Gilson Souza <gilsoncsouzajr@gmail.com>
2023-06-14 23:23:37 -0300

    update index stock

--- app/Views/stock/index.php
+++ app/Views/stock/index.php
@@ -7,14 +7,21 @@
     <h2><?= $tittle ?></h2>
     
     <div class="row card-2 py-3 my-3">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <input type="text" name="search" id="search" class="form-control" placeholder="Buscar" style="background-color: transparent;">
         </div>
+        
+        <div class="col-md-4 btn-group">
+            <button class="btn btn-success">Exportar arquivo CSV</button>
+            <button class="btn btn-success">Importar</button>
+        </div>
+        
         <div class="col-md-4 btn-group">
             <button class="btn btn-success">Pesquisar</button>
             <button class="btn btn-success">Filtros</button>
             <a class="btn btn-success" href="<?= $baseRoute ?>/novo"><?= $addButtonText ?></a>
         </div>
+        
     </div>
 
     <p>
@@ -26,9 +33,9 @@
             <thead>
                 <tr>
                     <th scope="col">Nome</th>
-                    <th scope="col">Contato</th>
-                    <th scope="col">Plano</th>
-                    <th scope="col">Endereço</th>
+                    <th scope="col">SKU</th>
+                    <th scope="col">Quantidade</th>
+                    <th scope="col">Preço</th>
                     <th scope='col'>Ações</th>
                 </tr>
             </thead>
