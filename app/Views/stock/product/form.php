6a199e58ee16df6d4ce363832c9ca4798fe4b0de
Gilson Souza <gilsoncsouzajr@gmail.com>
2023-06-14 23:34:43 -0300

    update stock paste

--- app/Views/stock/product/form.php
+++ app/Views/stock/product/form.php
@@ -1,21 +1,3 @@
-<!-- 
-    campos para o form
-    sku 
-    nome
-    descricao
-    quantidade
-    preco
-    categoria
-    marca
-    modelo
-    fornecedor
-    data de compra
-    data de fabricacao
-    data de atualizacao
-    data de criacao
-    data de exclusao
- -->
-
 
  <?= $this->extend('layout/index') ?>
 
