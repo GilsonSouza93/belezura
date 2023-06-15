48fce5beb6ba404c657ee71a75904fd00755bc01
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2023-06-14 22:20:02 -0300

    update camps

---
+++ app/Views/stock/form.php
@@ -0,0 +1,17 @@
+<!-- 
+    campos para o form
+    sku 
+    nome
+    descricao
+    quantidade
+    preco
+    categoria
+    marca
+    modelo
+    fornecedor
+    data de compra
+    data de fabricacao
+    data de atualizacao
+    data de criacao
+    data de exclusao
+ -->
