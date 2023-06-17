ac4b51eea53a7ef3a60730f6999056bc39567c38
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2023-06-17 01:45:13 -0300

    update

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -3,28 +3,64 @@
 <?= $this->section('content') ?>
 
 <div class="card bg-black m-2 p-4">
-    <h1 class="text-white">
+    <h2>
         <?= $tittle ?>
-    </h1>
+    </h2>
 
     <div class="row" style="height: 400PX">
         <div class="col-8">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15792.385967731489!2d-35.99380153693221!3d-8.293195784465718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98b96e8d7fd6d%3A0xa30a5c7c9e363ef5!2sCaruaru%20-%20Picada%2C%20Caruaru%20-%20PE%2C%2055032-250!5e0!3m2!1spt-BR!2sbr!4v1685933291213!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0; border-radius: 15px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="col-4">
-            <div class="container">
-                <div class="row row-cols-2" style="height: 400px;">
-                    <div class="col bg-primary bg-gradient">Contador 1</div>
-                    <div class="col bg-success bg-gradient">Contador 2</div>
-                    <div class="col bg-orange bg-gradient">Contador 3</div>
-                    <div class="col bg-gradient" style="background-color: #ff914d;">Contador 4</div>
+            <div class="container" style="height: 390px;">
+                <div class="row h-50" style="margin-bottom: 10px;">
+                    <div class="col gradient-1">
+                        <div class="d-flex justify-content-around align-items-center h-100"> 
+                            <div class="d-flex">
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h5 class="mb-0 text-white">Clientes</h5>
+                                    <h1 class="me-2 mb-0 text-white">10.000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col gradient-2">
+                        <div class="d-flex justify-content-around align-items-center h-100"> 
+                            <div class="d-flex">
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h5 class="mb-0 text-white">Olts</h5>
+                                    <h1 class="me-2 mb-0 text-white">500</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row h-50">
+                    <div class="col gradient-3">
+                        <div class="d-flex justify-content-around align-items-center h-100"> 
+                            <div class="d-flex">
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h5 class="mb-0 text-white">Entrada</h5>
+                                    <h1 class="me-2 mb-0 text-white">R$ 9000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col gradient-4">
+                        <div class="d-flex justify-content-around align-items-center h-100"> 
+                            <div class="d-flex">
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h5 class="mb-0 text-white">Notificações</h5>
+                                    <h1 class="me-2 mb-0 text-white">10</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<div class="card bg-black m-2 p-4">
-    
 
 <?= $this->endSection() ?>
--- app/Views/layout/index.php
+++ app/Views/layout/index.php
@@ -6,26 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= esc($tittle) ?></title>
     <link href="<?= base_url('assets/css/bootstrap_5.3.min.css') ?>" rel="stylesheet">
-
     <style>
         :root {
-            --border-radius: 10px;
-            --background-color: #000;
+            --border-radius: 15px;
+            --bs-body-bg-rgb: 0, 0, 0;
             --card-background-color-primary: #212529;
             --card-background-color-secondary: #1E1E1E;
+
+            --bs-body-bg-rgb: 33,37,41;
+            --bs-primary-rgb: 13,110,253;
+            --bd-accent-rgb: 255,228,132;
+            --bd-violet-rgb: 112.520718,44.062154,249.437846;
+            --bd-pink-rgb: 214,51,132;
         }
+
         body {
-            background-color: var(--background-color);
+            background-image: linear-gradient(180deg, rgba(var(--bs-body-bg-rgb), 0.01), rgba(var(--bs-body-bg-rgb), 1) 85%),radial-gradient(ellipse at top left, rgba(var(--bs-primary-rgb), 0.5), transparent 50%),radial-gradient(ellipse at top right, rgba(var(--bd-accent-rgb), 0.5), transparent 50%),radial-gradient(ellipse at center right, rgba(var(--bd-violet-rgb), 0.5), transparent 50%),radial-gradient(ellipse at center left, rgba(var(--bd-pink-rgb), 0.5), transparent 50%);
+
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            background-position: center;
         }
+
         .card {
-            background-color: var(--card-background-color-primary) !important;
             border-radius: var(--border-radius);
+            background: rgba(0, 0, 0, 0.5) !important;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            border: 1px solid rgba(33, 37, 41, 0.2);
         }
 
         .card-2 {
-            background-color: var(--card-background-color-secondary) !important;
+            border-radius: var(--border-radius);
+            background: rgba(0, 0, 0, 0.5) !important;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            border: 1px solid rgba(33, 37, 41, 0.2);
+        }
+
+        .gradient-1 {
+            background: linear-gradient(to right, #ffbd59, #ff914d);
+            margin: 0 5px;
             border-radius: var(--border-radius);
         }
+
+        .gradient-2 {
+            background: linear-gradient(to right, #00bf63, #7ed957);
+            margin: 0 5px;
+            border-radius: var(--border-radius);
+        }
+
+        .gradient-3 {
+            background: linear-gradient(to right, #ff4e50, #f9d423);
+            margin: 0 5px;
+            border-radius: var(--border-radius);
+        }
+
+        .gradient-4 {
+            background: linear-gradient(to right, #00aeef, #5271ff);
+            margin: 0 5px;
+            border-radius: var(--border-radius);
+        }
+
     </style>
 </head>
 
@@ -39,12 +80,12 @@
                 <?php foreach ($navigation_bar_items as $item): ?>
                     <?php if ($item['show_subitems']): ?>
                         <div class="nav-item mx-4 dropdown">
-                            <a class="nav-link dropdown-toggle text-white" href="<?= $item['href'] ?>" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="<?= $item['href'] ?>" data-bs-toggle="dropdown">
                                 <?= $item['title'] ?>
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-dark mt-2" aria-labelledby="navbarDarkDropdownMenuLink" style="background: rgba(33,37,41, 0.7); backdrop-filter: blur(5px); border-radius: 10px; box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); border: 2px solid <?= $item['color'] ?>;">
+                            <ul class="dropdown-menu dropdown-menu-dark mt-4 px-4" style="background: rgba(0,0,0, 0.45); backdrop-filter: blur(7px); border-radius: 15px; border: 2px solid <?=$item['color'] ?>;">
                                 <?php foreach ($item['subitems'] as $subitem): ?>
-                                    <li><a class="dropdown-item text-white" href="<?= $subitem['href'] ?>"><?= $subitem['title'] ?></a></li>
+                                <li><a class="dropdown-item text-white rounded my-1" href="<?= $subitem['href'] ?>"><?= $subitem['title'] ?></a></li>
                                 <?php endforeach ?>
                             </ul>
                         </div>
