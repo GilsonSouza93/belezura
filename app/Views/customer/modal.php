47c9d13f3e99241b2df2ee873dd978c7f67e187b
Gilson Souza <102156674+GilsonSouza93@users.noreply.github.com>
2023-08-15 09:52:51 -0300

    delete modal

--- app/Views/customer/modal.php
+++
@@ -1,8 +0,0 @@
-<form>
-    <div class="row">
-        <div class="col">
-            <label class="form-label" for="name">Nome:</label>
-            <input type="text" name="name" id="name" class="form-control" placeholder="Nome da marca" style="background-color: transparent;">
-        </div>
-    </div>
-</form>
