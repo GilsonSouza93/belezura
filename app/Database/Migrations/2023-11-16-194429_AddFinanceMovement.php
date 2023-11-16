adb659e2cc9088681fe24ce9bc169a7d68439d08
Gilsiin <145351739+Gilsiin@users.noreply.github.com>
2023-11-16 17:45:54 -0300

    adicionei o finance movement

---
+++ app/Database/Migrations/2023-11-16-194429_AddFinanceMovement.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddFinanceMovement extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                 'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+
+            'Recive_point' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+
+            'register_date' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+
+            'date_competence' => [
+                'type' => 'DATE',
+                'null' => false,
+            ],
+
+            'payment_method' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+
+            'history' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+            
+            'user' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30,
+                'null' => false,
+            ],
+
+           // 'accounts_plans' []
+
+           'enter' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+           ],
+
+           'exit' => [
+            'type' => 'VARCHAR',
+            'constraint' => 255,
+            'null' => false,
+           ],
+
+           'actions' => [
+            'type' => 'VARCHAR',
+            'constraint' => 255,
+            'null' => false,
+           ],
+           ]); 
+           $this->forge->addPrimaryKey('id'); 
+
+           $this->forge->CreateTable('AddFinanceMovement');
+
+    }
+
+    public function down()
+    {
+        $this->forge->DropTable('AddFinanceMovement');
+    }
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '071661b71a6e7a31ddf0f7d8493464b377ec4135',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '071661b71a6e7a31ddf0f7d8493464b377ec4135',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
