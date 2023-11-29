b573290e8b2cf68f34cf45169026c7c315f71a1f
Gilson Souza <102156674+GilsonSouza93@users.noreply.github.com>
2023-11-29 16:42:37 -0300

    Charts financial dashboard

--- app/Views/financial/index.php
+++ app/Views/financial/index.php
@@ -2,12 +2,18 @@
 
 <?= $this->section('content') ?>
 
-<div class="row d-flex justify-content-around">
-    <div class="col-md-4 card md-3">
-        <canvas id="payments" height="20" width="40" ></canvas>
+<div class="row d-flex mt-5 justify-content-center">
+    <div class="col-md-3 card mx-3">
+      <label for="payments" class="mt-2">Receita</label>
+      <canvas id="payments" height="300"></canvas>
     </div>
-    <div class="col-md-2 card md-3">
-        <canvas id="pizza"></canvas>
+    <div class="col-md-3 card mx-3">
+      <label for="payments" class="mt-2">Clientes</label>
+      <canvas id="customers"></canvas>
+    </div>
+    <div class="col-md-3 card mx-3">
+      <label for="payments" class="mt-2">POPs</label>
+        <canvas id="popChart"></canvas>
     </div>
 </div>
 
@@ -21,16 +27,16 @@
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Junho', 'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+      labels: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado','Domingo'],
       datasets: [
         {
-        label: 'Receita',
-        data: [12, 19, 3, 5, 2, 3,8,6,2,8,4,9,2],
+        label: 'Entrada',
+        data: [12, 19, 3, 5, 2, 3,8],
         borderWidth: 1
       },
         {
-        label: 'Despesa',
-        data: [6, 7, 2, 2, 1, 2,4,3,1,4,2,5,1],
+        label: 'Saída',
+        data: [6, 7, 2, 2, 1, 2,4,3],
         borderWidth: 1
       },
     ]
@@ -43,15 +49,15 @@
       }
     }
   });
-  const pizza = document.getElementById('pizza');
+  const customers = document.getElementById('customers');
 
-  new Chart(pizza, {
+  new Chart(customers, {
     type: 'pie',
     data: {
-      labels: ['Janeiro', 'Fevereiro', 'Março'],
+      labels: ['Ativo', 'Suspenso', 'Reduzido','Cancelado'],
       datasets: [{
-        label: 'Quantidade de clientes cadastrados no mês',
-        data: [12, 19, 3],
+        label: 'Quantidade de clientes',
+        data: [120, 17, 29, 7],
         borderWidth: 1
       }]
     },
@@ -64,6 +70,19 @@
       }
     }
   });
+
+  var popsChart = document.getElementById('popChart').getContext('2d');
+var myChart = new Chart(popsChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Pernambuco', 'São Paulo', 'Minas Gerais'],
+        datasets: [{
+          label:'POPS',
+            data: [30, 50, 20],
+            //  
+        }]
+    }
+});
 </script>
 
 <?= $this->endSection() ?>
