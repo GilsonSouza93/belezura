3fd2975108c7d68b4d7a16370b86a375cec031a3
S3rgioHe <102156674+GilsonSouza93@users.noreply.github.com>
2023-11-30 17:09:27 -0300

    update

--- app/Views/paymentpoint/index.php
+++ app/Views/paymentpoint/index.php
@@ -5,42 +5,39 @@
 <div class="p-4">
 
     <h2><?= $tittle ?></h2>
-    
-    <div class="row py-3 my-3">
-        <div class="col-md-4">
-            <input type="text" name="search" id="search" class="form-control" placeholder="Buscar" style="background-color: transparent;">
+
+    <div class="row card-2 py-3 my-3">
+        <div class="col-md-8">
+            <input type="text" id="search" class="form-control" placeholder="Buscar" style="background-color: transparent;">
         </div>
-        
         <div class="col-md-4 btn-group">
-            <button class="btn btn-success">Exportar Arquivo CSV</button>
-            <button class="btn btn-success">Importar</button>
-        </div>
-        
-        <div class="col-md-4 btn-group">
-            <button class="btn btn-success">Pesquisar</button>
-            <button class="btn btn-success" onclick="openModalIpPool()" >Filtros</button>
+            <button class="btn btn-success" id="searchBtn">Pesquisar</button>
+            <button class="btn btn-success" id="accountFilter">Filtros</button>
             <a class="btn btn-success" href="<?= $baseRoute ?>/novo"><?= $addButtonText ?></a>
         </div>
-        
     </div>
 
-    
-    <div id="tableIppool" class="collapsed p-4 justify-content ">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                <th scope="col">Nome</th>
-                    <th scope="col">Portador</th>
-                    <th scope="col">Empresa</th>
-                    <th scope="col">Disponibilidade</th>
-                    <th scope="col">Ativos</th>
-                    <th scope="col"> </th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </div>
+    <!-- tabela gerada por script -->
+    <div id="tableDiv"></div>
+
 </div>
-
 <?= $this->endSection() ?>
+
+
+<?= $this->section('script') ?>
+
+<script>
+    const renderTableOptions = {
+        urlFetch: window.location.href + '/search',
+        tableDiv: document.getElementById('tableDiv'),
+        theadElements: ['Portador', 'Empresa','Ativos', 'ações'],
+        tbodyElements: ['carrier', 'companies', 'active', ['edit', 'delete']],
+        searchField: document.getElementById('search'),
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        advancedSearchEngine(renderTableOptions);
+    });
+</script>
+
+<?= $this->endSection() ?> 
